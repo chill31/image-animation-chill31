c17178b5116498885f7ecca1c7c327d10537a02d
chill31 <89910163+chill31@users.noreply.github.com>
2024-07-11 18:28:39 +0530

    change button text on click

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const imgWrapper = document.querySelector(".image-wrapper");
 
 function animate() {
   imgWrapper.classList.toggle("done");
+  btn.textContent === 'DEFAULT' ? btn.textContent = 'ACTIVATE' : btn.textContent = 'DEFAULT';
 }
 
 btn.addEventListener("click", animate);
